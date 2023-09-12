8b55557b21dd01a7ac0e3ab2a68a736254bbdd9c
Charles Josiah Rusch Alandt <charles.alandt@gmail.com>
2023-09-12 07:09:38 -0300

    Create s3_block_presigned_log_athena.sql

---
+++ aws/s3_block_presigned_log_athena.sql
@@ -0,0 +1,40 @@
+## https://repost.aws/knowledge-center/analyze-logs-athena
+## Amazon S3 stores server access logs as objects in an S3 bucket. Use Athena to quickly analyze and query server access logs.
+  
+CREATE EXTERNAL TABLE `s3_access_logs_db.mybucket_logs`(
+  `bucketowner` STRING, 
+  `bucket_name` STRING, 
+  `requestdatetime` STRING, 
+  `remoteip` STRING, 
+  `requester` STRING, 
+  `requestid` STRING, 
+  `operation` STRING, 
+  `key` STRING, 
+  `request_uri` STRING, 
+  `httpstatus` STRING, 
+  `errorcode` STRING, 
+  `bytessent` BIGINT, 
+  `objectsize` BIGINT, 
+  `totaltime` STRING, 
+  `turnaroundtime` STRING, 
+  `referrer` STRING, 
+  `useragent` STRING, 
+  `versionid` STRING, 
+  `hostid` STRING, 
+  `sigv` STRING, 
+  `ciphersuite` STRING, 
+  `authtype` STRING, 
+  `endpoint` STRING, 
+  `tlsversion` STRING,
+  `accesspointarn` STRING,
+  `aclrequired` STRING)
+ROW FORMAT SERDE 
+  'org.apache.hadoop.hive.serde2.RegexSerDe' 
+WITH SERDEPROPERTIES ( 
+  'input.regex'='([^ ]*) ([^ ]*) \\[(.*?)\\] ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) (\"[^\"]*\"|-) (-|[0-9]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) (\"[^\"]*\"|-) ([^ ]*)(?: ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*) ([^ ]*))?.*$') 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://logs-s3-566080532795/'
